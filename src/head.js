3e137997d00625fa77769c8ed99ceac6e770d79d
Arsenic <ar3en1c@outlook.com>
2023-04-07 18:39:26 +0330

    complete header secction

--- src/head.js
+++ src/head.js
@@ -2,12 +2,23 @@
 import React from "react";
 import './head.scss';
 let header = () => {
-	return(
-    <header className="head">
-        <div className="hello">
-            Hello World
-        </div>
-    </header>)
+    return (
+        <header className="head">
+            <div className="hello">
+                <div className="left-secctions">
+                    <div>جستجو</div>
+                    <div>پروفایل</div>
+                </div>
+                <div className="right-secctions">
+                    <div style={{fontSize : '25px' , fontFamily: 'arial' , paddingTop: '15px'}}>لوگو</div>
+                    <div>خانه</div>
+                    <div>فیلم ها</div>
+                    <div>سریال ها</div>
+                    <div>دسته بندی</div>
+                </div>
+            </div>
+        </header>
+    )
 }
 
 export default header;
