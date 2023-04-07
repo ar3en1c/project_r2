1371cbe7b356f44f738e2947f03a9ff595fc3382
Arsenic <ar3en1c@outlook.com>
2023-04-07 17:42:24 +0330

    first hello world app

---
+++ src/head.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import './head.scss';
+let header = () => {
+	return(
+    <header className="head">
+        <div className="hello">
+            Hello World
+        </div>
+    </header>)
+}
+
+export default header;
