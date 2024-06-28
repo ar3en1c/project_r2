fa482a44f63080e11eab1ec4839a09e518acddf3
Arsenic <ar3en1c@outlook.com>
2024-06-28 17:36:14 +0330

    Updates

---
+++ series-api.php
@@ -0,0 +1,14 @@
+<?php
+$db = new PDO('sqlite:' . __DIR__ . '/mov.db');
+
+$qgetdata = $db->query("SELECT * FROM SERIES");
+$getdata = $qgetdata->fetchAll(PDO::FETCH_ASSOC);
+$option = [];
+foreach ($getdata as $value) {
+    $option[] = [$value["id"] , $value["name"] , $value["raiting"] , $value["cover"] , $value["genras"] , $value["year"] , $value["summery"] , $value["link"]];
+}
+echo json_encode($option);
+
+$db = null;
+header('Access-Control-Allow-Origin: http://localhost:3000');
+?>
